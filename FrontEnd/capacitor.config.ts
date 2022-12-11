e192e40e68b66d45976fd17b8a864743acc43963
EliasXabs <eliasxabs@gmail.com>
2022-12-11 18:29:33 +0200

    initial commit

---
+++ FrontEnd/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.bookme',
+  appName: 'bookme',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
