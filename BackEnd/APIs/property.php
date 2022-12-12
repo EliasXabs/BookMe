92cd442c0933be33fea0218fcc8693fc4af73add
EliasXabs <eliasxabs@gmail.com>
2022-12-12 18:01:03 +0200

    Created addproperty API

---
+++ BackEnd/APIs/property.php
@@ -0,0 +1,27 @@
+<?php
+
+include("connection.php");
+
+
+// If post request is sent we create a new property
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $price = $_POST["price"];
+    $pictures = $_POST["pictures"];
+    $country = $_POST["country"];
+    $city = $_POST["city"];
+    $category = $_POST["catetgory"];
+    $user_id = $_POST["user_id"];
+
+    $sql = "INSERT INTO properties(price, pictures, country, city, category, owner_id) VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+
+    mysqli_stmt_bind_param($stmt, "issssi", $price, $pictures, $country, $city, $category, $user_id);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+
+    $obj = array("status" => 200, "message" => "Property created");
+    echo(json_encode($obj));
+    exit();
+}
