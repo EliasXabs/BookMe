ad1889537ac274281586701b34c41c92667a9c1a
EliasXabs <eliasxabs@gmail.com>
2022-12-12 18:36:22 +0200

    Added get properties by userid

--- BackEnd/APIs/property.php
+++ BackEnd/APIs/property.php
@@ -71,3 +71,20 @@ if($_SERVER["REQUEST_METHOD"] == "GET" && !empty($_GET["country"])){
     echo(json_encode($properties));
     exit();
 }
+
+//if get request is sent with a user_id parameter we return all properties with that user_id
+if($_SERVER["REQUEST_METHOD"] == "GET" && !empty($_GET["user_id"])){
+    $user_id = $_GET["user_id"];
+    $sql = "SELECT * FROM properties WHERE owner_id = ?";
+    $stmt = mysqli_stmt_init($connection);
+    mysqli_stmt_prepare($stmt, $sql);
+    mysqli_stmt_bind_param($stmt, "i", $user_id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $properties = array();
+    while($row = mysqli_fetch_assoc($result)){
+        $properties[] = $row;
+    }
+    echo(json_encode($properties));
+    exit();
+}
