24ebc4d7a229a7e1bdae0318ded3198c4803c0c3
EliasXabs <eliasxabs@gmail.com>
2022-12-13 21:15:51 +0200

    Linked splash to onboard after 5s

--- FrontEnd/src/app/splash-screen/splash-screen.page.ts
+++ FrontEnd/src/app/splash-screen/splash-screen.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, UrlTree } from '@angular/router';
+
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.page.html',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashScreenPage implements OnInit {
 
-  constructor() { }
+  constructor(public router:Router) { 
+  }
 
   ngOnInit() {
+    let view: string | UrlTree;
+    const checkView = localStorage.getItem('pageDisplayed');
+    if (checkView == "true") {
+        view = "/home";
+    }
+    else {
+        view = "/onboard-1";
+    }
+    localStorage.setItem('pageDisplayed', "true");
+    
+    setTimeout(() => {
+      this.router.navigateByUrl(view);
+    }, 5000);
   }
 
 }
