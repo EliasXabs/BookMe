0a7da2d51355b2a750e950c278f4f96fbb140a21
EliasXabs <eliasxabs@gmail.com>
2022-12-14 15:47:08 +0200

    Added functionalities

--- FrontEnd/src/app/signup/signup.page.ts
+++ FrontEnd/src/app/signup/signup.page.ts
@@ -8,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class SignupPage implements OnInit {
 
   uinvalue: any;
-  
-  constructor() { }
+  einvalue: any;
+  pinvalue: any;
+  cinvalue: any;
+  namehelp: boolean = false;
+  emailhelp: boolean = false;
+  passhelp: boolean = false;
+  nameerror: boolean = true;
+  emailerror: boolean = true;
+  passmatch: boolean = true;
+  signbtn: boolean = true;
+
+  constructor() { 
+
+  }
   
   ngOnInit() {
   }
@@ -17,7 +29,61 @@ export class SignupPage implements OnInit {
   checkusername(){
     // check if uinvalue is atleast 3 characters long
     if(this.uinvalue.length >= 3){
-      
+      this.namehelp = true;
+    }
+    else{
+      this.namehelp = false;
+    }
+    this.checkall();
+  }
+
+  checkemail(){
+    // check if email has at least 1 @ and 1 .\
+    if(this.einvalue.includes('@') && this.einvalue.includes('.')){
+      this.emailhelp = true;
+    }
+    else{
+      this.emailhelp = false;
+    }
+    this.checkall();
+  }
+
+  checkpass(){
+    if(this.pinvalue.length >= 8){
+      this.passhelp = true;
+      this.passmatch =false;
+    }
+    else{
+      this.passhelp = false;
+    }
+    this.checkconfirm();
+    this.checkall();
+  }
+  checkconfirm(){
+    if(this.cinvalue == this.pinvalue){
+      this.passmatch = true;
+    }
+    else{
+      this.passmatch = false;
+    }
+    this.checkall();
+  }
+
+  checkall(){
+    console.log(this.namehelp);
+    console.log(this.emailhelp);
+    console.log(this.passhelp);
+    console.log(this.passmatch);
+    if(
+      this.namehelp == true &&
+      this.emailhelp == true &&
+      this.passhelp == true &&
+      this.passmatch == true
+    ){
+      this.signbtn = false;
+    }
+    else{
+      this.signbtn = true;
     }
   }
 
