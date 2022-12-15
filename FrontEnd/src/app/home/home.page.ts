2278ec51cec38505c1cfba0b3f586a479b124f29
EliasXabs <eliasxabs@gmail.com>
2022-12-15 22:04:19 +0200

    Home built no linking

--- FrontEnd/src/app/home/home.page.ts
+++ FrontEnd/src/app/home/home.page.ts
@@ -8,9 +8,14 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   profile: any ;
+  properties: {id: number, title: string, price: number, country: string, image: string}[] = [];
 
   constructor() {
     this.profile = "data:image/png;base64,"+ localStorage.getItem('pp');
+    this.properties.push({id: 1, title: "test", price: 100, country: "test", image: "../../assets/res/SplashImage.png"});
+    this.properties.push({id: 2, title: "test3", price: 200, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.properties.push({id: 3, title: "test2", price: 1100, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.properties.push({id: 4, title: "test4", price: 1000, country: "test", image: "/assets/res/onboardHouse.png"});
   }
   
   ngoninit() {
