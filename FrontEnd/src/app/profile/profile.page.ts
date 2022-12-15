c556aaca4cea5334dd1951b7cd534c60a03fda5e
EliasXabs <eliasxabs@gmail.com>
2022-12-15 21:32:37 +0200

    stars page without linking

--- FrontEnd/src/app/profile/profile.page.ts
+++ FrontEnd/src/app/profile/profile.page.ts
@@ -11,15 +11,15 @@ export class ProfilePage implements OnInit {
 
   profile: any ;
   name: any;
-  favorites: {id: number, title: string, price: number, country: string, image: string}[] = [];
+  owned: {id: number, title: string, price: number, country: string, image: string}[] = [];
   
   constructor(private service: FavoritesService) {
     this.profile = "data:image/png;base64,"+ localStorage.getItem('pp');
     this.name = localStorage.getItem('username');
-    this.favorites.push({id: 1, title: "test", price: 100, country: "test", image: "../../assets/res/SplashImage.png"});
-    this.favorites.push({id: 2, title: "test3", price: 200, country: "test", image: "/assets/res/onboardHouse.png"});
-    this.favorites.push({id: 3, title: "test2", price: 1100, country: "test", image: "/assets/res/onboardHouse.png"});
-    this.favorites.push({id: 4, title: "test4", price: 1000, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.owned.push({id: 1, title: "test", price: 100, country: "test", image: "../../assets/res/SplashImage.png"});
+    this.owned.push({id: 2, title: "test3", price: 200, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.owned.push({id: 3, title: "test2", price: 1100, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.owned.push({id: 4, title: "test4", price: 1000, country: "test", image: "/assets/res/onboardHouse.png"});
   }
   
   ngOnInit() {
--- FrontEnd/src/app/stars/stars.page.ts
+++ FrontEnd/src/app/stars/stars.page.ts
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class StarsPage implements OnInit {
 
   profile: any ;
+  stars: {id: number, title: string, price: number, country: string, image: string}[] = [];
 
   constructor() { 
     this.profile = "data:image/png;base64,"+ localStorage.getItem('pp');
+    this.stars.push({id: 1, title: "test", price: 100, country: "Lebanon", image: "../../assets/res/SplashImage.png"});
+    this.stars.push({id: 2, title: "test3", price: 200, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.stars.push({id: 3, title: "test2", price: 1100, country: "test", image: "/assets/res/onboardHouse.png"});
+    this.stars.push({id: 4, title: "test4", price: 1000, country: "test", image: "/assets/res/onboardHouse.png"});
   }
 
   ngOnInit() {
